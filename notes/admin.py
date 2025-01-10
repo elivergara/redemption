a58055ee277ab315dc98b5349bc2f17333f9fac1
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2025-01-09 20:00:36 -0700

    Added NOtes to admin page

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Note
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'author')
