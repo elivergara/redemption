2ed05b0b9115c19dfcccf16479ccf3e5869d6563
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2025-01-23 10:32:22 -0700

    updated css version to see if it affects cached style

--- home/static/serviceworker.js
+++ home/static/serviceworker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'eg-app-cache-v5.0'; // Increment on each update
+const CACHE_NAME = 'eg-app-cache-v5.1'; // Increment on each update
 const urlsToCache = [
   '/', // Cache the homepage
-  '/static/style.css?v=5.0', // Versioned CSS URL
+  '/static/style.css?v=5.1', // Versioned CSS URL
   '/static/js/main.js', // JS file (update as needed)
   '/static/icons/icon-192x192.png', // App icon
   '/static/icons/icon-512x512.png'  // App icon
